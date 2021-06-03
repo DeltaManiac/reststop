0451da3a76538f769b2b05c52652f2f890d0afd0
DeltaManiac <maxpaynered@gmail.com>
2021-06-03 22:47:35 +0530

    Added HTTP Methods

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,53 @@
-use druid::widget::prelude::*;
-use druid::widget::{Button, Flex, Label, TextBox};
-use druid::{AppLauncher, Application, Data, Lens, UnitPoint, WidgetExt, WindowDesc, Menu, WindowId, LocalizedString};
+use eframe::{egui,epi};
 use reqwest;
-use std::io::Read;
 
-#[derive(Debug, Data, Lens, Clone)]
+#[derive(Debug, Clone,Eq,PartialEq )]
+pub enum HTTPMethod {
+    PUT,
+    GET,
+    POST,
+    DELETE
+}
+impl Default for HTTPMethod{
+    fn default()-> HTTPMethod {
+       HTTPMethod::GET
+    }
+}
+#[derive(Debug, Clone, Default)]
 pub struct UI {
     url: String,
     response: String,
-    #[data(ignore)]
     headers: Option<reqwest::header::HeaderMap>,
+    http_method: HTTPMethod,
 }
 
-impl Default for UI {
-    fn default() -> Self {
-        UI {
-            url: String::from("http://httpbin.org/get"),
-            response: String::from(""),
-            headers: None,
-        }
+impl epi::App for UI{
+    fn name(&self)-> &str{
+       "RestStop"
+    }
+    fn update(&mut self, ctx:&egui::CtxRef, _frame: &mut epi::Frame<'_>){
+        egui::CentralPanel::default().show(ctx, |ui|{
+            ui.heading("Rest Stop");
+            ui.separator();
+            ui.horizontal(|ui| {
+                egui::ComboBox::from_label("").selected_text(format!("{:?}",&mut self.http_method)).show_ui(ui,|ui| {
+                    ui.selectable_value(&mut self.http_method ,HTTPMethod::GET,"GET");
+                    ui.selectable_value(&mut self.http_method ,HTTPMethod::PUT,"PUT");
+                    ui.selectable_value(&mut self.http_method ,HTTPMethod::POST,"POST");
+                    ui.selectable_value(&mut self.http_method ,HTTPMethod::DELETE,"DELETE");
+                });
+                ui.text_edit_singleline(&mut self.url.clone());
+                ui.button("Go")
+            });
+            ui.separator();
+        });
+
     }
 }
+
 
 fn main() {
-    let main_window = WindowDesc::new(build_root_widget()).menu(make_menu).title("RestStop");
-    let init = UI::default();
-    AppLauncher::with_window(main_window)
-        .log_to_console()
-        .launch(init)
-        .expect("Phail");
-}
-
-fn build_root_widget() -> impl Widget<UI> {
-    let label = Label::new(|data: &UI, _env: &Env| {
-        if data.url.is_empty() {
-            "Hello anybody!?".to_string()
-        } else {
-            format!("Hello {}!", data.url)
-        }
-    })
-    .with_text_size(32.0);
-
-    // a textbox that modifies `name`.
-    let textbox = TextBox::new()
-        .with_placeholder("Who are we greeting?")
-        .with_text_size(18.0)
-        // .fix_width(TEXT_BOX_WIDTH)
-        .lens(UI::url);
-
-    // arrange the two widgets vertically, with some padding
-    Flex::column()
-        .with_child(label)
-        // .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_child(textbox)
-        .align_vertical(UnitPoint::LEFT)
-}
-
-
-#[allow(unused_assignments, unused_mut)]
-fn make_menu<T: Data>(_window: Option<WindowId>, _data: &UI, _env: &Env) -> Menu<T> {
-    let mut base = Menu::empty();
-    #[cfg(target_os = "macos")]
-    {
-        base = base.entry(druid::platform_menus::mac::application::default())
-    }
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    {
-        base = base.entry(druid::platform_menus::win::file::default());
-    }
-    base
+    let app = UI::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
